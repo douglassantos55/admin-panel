700f01a99a24009d1aacc7f58e6a76e51e20ca2d
Douglas Santos <hiperhavy_doug@hotmail.com>
2022-04-14 18:40:12 -0300

    feat: input

---
+++ resources/js/Components/Input.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="mb-3" :class="$attrs.class">
+        <label :for="id" class="form-label" v-if="label">{{ label }}</label>
+        <input
+            :id="id"
+            v-bind="$attrs"
+            class="form-control"
+            :class="{ 'is-invalid': !!error }"
+            @input="$emit('update:modelValue', $event.target.value)"
+        />
+        <div class="invalid-feedback" v-if="error">{{ error }}</div>
+    </div>
+</template>
+
+<script>
+export default {
+    inheritAttrs: false,
+    emits: [
+        'update:modelValue',
+    ],
+    props: {
+        modelValue: {
+            type: String,
+            required: true,
+        },
+        error: {
+            type: Array,
+            required: false,
+        },
+        label: {
+            type: String,
+            required: false,
+        },
+    },
+    computed: {
+        id() {
+            return `${this.label.toLowerCase()}_${parseInt(Math.random() * 100)}`;
+        }
+    },
+}
+</script>
