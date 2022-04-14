94271f04bf218ede7f1114bfe9dba175e48c19f1
Douglas Santos <hiperhavy_doug@hotmail.com>
2022-04-14 18:23:41 -0300

    feat: layout

---
+++ resources/js/Pages/Layout.vue
@@ -0,0 +1,49 @@
+<script setup>
+import { Link } from '@inertiajs/inertia-vue3'
+</script>
+
+<template>
+    <header class="navbar navbar-dark bg-dark px-3 sticky-top">
+        <Link href="/" class="navbar-brand">Reconcip</Link>
+
+        <div class="navbar-nav">
+            <div class="nav-item">
+                <Link href="/auth/logout" class="nav-link">Sair</Link>
+            </div>
+        </div>
+    </header>
+
+    <div class="container-fluid">
+        <div class="row">
+            <nav class="col-md-3 col-lg-2 d-md-block bg-light sidebar">
+                <div class="position-sticky pt-3">
+                    <ul class="nav flex-column">
+                        <li class="nav-item">
+                            <Link href="/" class="nav-link">Dashboard</Link>
+                        </li>
+
+                        <li class="nav-item">
+                            <Link href="/customers" class="nav-link">Clientes</Link>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+
+            <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4 py-3">
+                <slot />
+            </main>
+        </div>
+    </div>
+</template>
+
+<style>
+.sidebar {
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    z-index: 100;
+    padding: 48px 0 0;
+    box-shadow: inset -1px 0 0 rgb(0 0 0 / 10%);
+}
+</style>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,16 @@
 import { createApp, h } from 'vue'
 import { createInertiaApp } from '@inertiajs/inertia-vue3'
 import { InertiaProgress } from '@inertiajs/progress'
+import Layout from './Pages/Layout.vue'
 
 InertiaProgress.init()
 
 createInertiaApp({
-  resolve: name => require(`./Pages/${name}`),
+  resolve: name => {
+      const page = require(`./Pages/${name}`).default
+      page.layout = page.layout || Layout
+      return page
+  },
   setup({ el, App, props, plugin }) {
     createApp({ render: () => h(App, props) })
       .use(plugin)
